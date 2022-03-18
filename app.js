b906d1740854db9fbd5e4dfa469b45fef0b357e1
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-18 15:40:36 +0900

    객체 이용 계산기

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  pow: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(3, 4);
+calculator.minus(3, 4);
+calculator.divide(9, 3);
+calculator.pow(2, 5);
