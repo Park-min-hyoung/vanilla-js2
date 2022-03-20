410a3c93376264a9888fee80afdf5e4e944e587f
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-20 13:29:54 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+  const toDoli = document.createElement("li");
+  const toDospan = document.createElement("span");
+  toDospan.innerText = newTodo;
+  toDoli.appendChild(toDospan);
+  toDoList.appendChild(toDoli);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
-  console.log(toDoInput.value);
   toDoInput.value = "";
-  console.log(newTodo, toDoInput.value);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
