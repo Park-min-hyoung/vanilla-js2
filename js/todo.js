08b51a4c0661b99a78c170d48961514142507d6a
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-21 12:17:38 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const removeli = event.target.parentElement;
+  removeli.remove();
+}
+
 function paintToDo(newTodo) {
   const toDoli = document.createElement("li");
   const toDospan = document.createElement("span");
   toDospan.innerText = newTodo;
+  const removeBtn = document.createElement("button");
+  removeBtn.innerText = "X";
+  removeBtn.addEventListener("click", deleteToDo);
+
   toDoli.appendChild(toDospan);
+  toDoli.appendChild(removeBtn);
   toDoList.appendChild(toDoli);
 }
 
